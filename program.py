b0ec9b43fcc87c5df594e1cd90d7c0974aabf394
Mark Dowell <markd4296@gmail.com>
2024-09-26 15:47:51 -0500

    complete w/ error handling

---
+++ program.py
@@ -0,0 +1,28 @@
+# take a video's total time "mm:ss" and convert it to seconds.
+# if the seconds is above 60 or higher, return -1.
+
+
+def get_time():
+
+    while True:
+        x = input(
+            "What's the length of the time of your video? (format should be 'mm:ss') "
+        )
+        try:
+            time_split = x.split(":")
+
+            if int(time_split[1]) < 60:
+                total_seconds = int(time_split[0]) * 60 + int(time_split[1])
+                print(f"Total seconds: {total_seconds}")
+                break
+            else:
+                print("-1")
+                break
+
+        except IndexError:
+            print("Please follow the correct time format (mm:ss): ")
+        except:
+            print("Something went wrong. Try using the correct format (mm:ss): ")
+
+
+get_time()
